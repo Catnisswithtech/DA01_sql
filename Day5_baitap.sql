5b25e894d869c0f2eea3a15abfcfabe48f7ecb3f
Catnisswithtech <maichitran1201@gmail.com>
2024-08-11 15:45:18 +0700

    Update Day5_baitap.sql

--- Day5_baitap.sql
+++ Day5_baitap.sql
@@ -7,3 +7,6 @@ SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION
 --Ex3
 
 --Ex4
+SELECT 
+ROUND(CAST(SUM(item_count * order_occurrences) / SUM(order_occurrences)as DECIMAL ,1) as mean
+FROM items_per_order;
